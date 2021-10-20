e1973ff046efa852fc226ad944c78e62e8937c1d
Ayub <58354447+azzzub@users.noreply.github.com>
2021-10-21 00:12:32 +0700

    Revert "fucking cors"

--- prven/src/components/pages/home/recap.jsx
+++ prven/src/components/pages/home/recap.jsx
@@ -1,36 +1,18 @@
 import { HStack, VStack } from "@chakra-ui/layout";
 import { Text } from "@chakra-ui/react";
 import Widget from "../../atoms/widget";
-import axios from "axios";
-import { useEffect, useState } from "react";
 
-function Recap() {
-  const [data, setData] = useState(null);
-  async function fetchData() {
-    try {
-      const result = await axios.get(
-        process.env.REACT_APP_ENDPOINT + "/latest_data"
-      );
-      setData(result.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+function Recap({ data }) {
   return (
     <VStack padding="2" alignItems="start" marginBottom="1">
       <Text fontSize="small">
-        Data terakhir diperbarui pada: <b>{data?.created_at || "..."}</b>
+        Data terakhir diperbarui pada: <b>{data?.latestUpdate || "datetime"}</b>
       </Text>
       <HStack width="100%">
-        <Widget title="Kelembaban Tanah" value={data?.soil_str || "..."} />
-        <Widget title="Ekstensometer" value={data?.extenso_str || "..."} />
+        <Widget title="Kelembaban Tanah" value="70%" />
+        <Widget title="Ekstensometer" value="100 cm" />
       </HStack>
-      <Widget title="Gyroscope" value={data?.gyro_str || "..."} />
+      <Widget title="Gyroscope" value="x: 10° y: 300°" />
     </VStack>
   );
 }
